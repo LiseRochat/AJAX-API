fc316dccb9b630a35712851997d85f7a8d111317
LiseRochat <93043649+LiseRochat@users.noreply.github.com>
2022-03-25 20:34:04 +0100

    Display Data in HTML

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ fetch('https://api.publicapis.org/entries')
 .then(function(response) {
   return response.json();
 })
+
+.then(function(myJson) {
+
+    //console.log(myJson);
+    console.log(myJson.entries);
+
+    for(i=0; i < myJson.entries.length; i++){
+
+        let clone =document.querySelector("#section").cloneNode(true);
+
+        clone.id = "newSection"+i;
+
+        clone.querySelector("h1").innerHTML = myJson.entries[i].API;
+        clone.querySelector("p.first").innerHTML = myJson.entries[i].Description;
+        clone.querySelector("p.last").innerHTML = myJson.entries[i].Category;
+        clone.querySelector("a").href = myJson.entries[i].Link;
+        clone.querySelector("a").innerHTML = myJson.entries[i].Link;
+        document.querySelector("body").append(clone);
+        
+   }
+
+});
